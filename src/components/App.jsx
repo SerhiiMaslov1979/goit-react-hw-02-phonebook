00a6632cee1688e73bbffac2ba7b1335a85df88a
SerhiiMaslov1979 <112109122+SerhiiMaslov1979@users.noreply.github.com>
2023-04-25 22:13:09 +0300

    Form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,66 @@
 import { Component } from "react";
-
-
 export class App extends Component {
+   state = {
+     contacts: [],
+     name: '',
+     number:'',
+    // filter: '',
+   };
+  
+  deleteTodo = todoid => {
 
-state = {
-  contacts: [],
-  name: ''
-}
+  };
 
+  //обновить состояние
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+   
 
+    this.setState({
+      [name]: value
+    })
+  }
+  // handleNameChange = event => {
+  //   // console.log(event.currentTarget.value);
 
+  //   this.setState({name: event.currentTarget.value});
+
+  // }
+  //  handleNumberChange = event => {
+  //   // console.log(event.currentTarget.value);
+
+  //   this.setState({number: event.currentTarget.value});
+
+  // }
+
+  render() {
+    return (
+
+      // <div>
+      //   <input type='text' value={this.state.filter}
+      //     // будет обновлять state при каждом вводе текста
+      //     onChange={this.handleInputChange} />
+      // </div>
+      
+      <div>
+        <form>
+          <label>
+            Name <input type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange} />
+          </label>
+          <label>
+            Number <input type="text"
+              name="number"
+              value={this.state.number}
+              onChange={this.handleChange} />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+      </div>
+)
+
+  }
 
 }
---
+++ src/components/ContactsList/ContactsList.jsx
@@ -0,0 +1,15 @@
+// export const ContactsList = ({contacts, onDeleteContacts}) => {
+//     console.log();
+//     return (
+//         <ul>
+//             <li>
+//                 <button>Vasya
+
+//                 </button>
+//             </li>
+//         </ul>
+//     );
+// }
+
+
+// export default ContactsList;
